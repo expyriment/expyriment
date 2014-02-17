588bb40f258d0c54fd9b16dc9186140a701b3100
lindemann09 <oliver@oliver-office.(none)>
2014-02-17 10:17:43 +0100

    touchscreen button

---
+++ expyriment/io/_touchscreen_buttonbox.py
@@ -0,0 +1,39 @@
+"""
+A touchscreen button box.
+
+This module contains a class implementing a text input box for user input.
+
+"""
+
+__author__ = 'Florian Krause <florian@expyriment.org>, \
+Oliver Lindemann <oliver@expyriment.org>'
+__version__ = ''
+__revision__ = ''
+__date__ = ''
+
+
+import defaults
+import expyriment
+from _input_output import Input
+
+
+class TextInput(Input):
+    """A class implementing a text input box."""
+
+    def __init__(self, fields):
+        """Create a touchscreen button box.
+
+		TODO 
+
+        """
+
+
+    def present(self, background_stimulus):
+        """
+
+		TODO 
+
+		"""
+		
+		pass
+
