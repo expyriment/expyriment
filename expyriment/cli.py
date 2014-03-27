6dc5cbb01e8d2dd71062cab77cbf86368a147868
Oliver Lindemann <lindemann09@googlemail.com>
2014-03-27 12:16:58 +0100

    long options

--- expyriment/cli.py
+++ expyriment/cli.py
@@ -37,6 +37,8 @@ Usage: python -m expyriment.cli [OPTIONS] [EXPYRIMENT SCRIPT]
       -h              Show this help
 """
 
+unknown_option = "Unknown option '{0}' (use --help for information)"
+
 def create_templet():
     template_file = '''#!/usr/bin/env python
 # -*- coding: utf-8 -*-
@@ -96,55 +98,72 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         for args in sys.argv[1:]:
             if args.startswith("-"):
-                #sort args (capital letters last)
-                arguments = list(args[1:])
-                arguments.sort(reverse=True)
-                for arg in arguments:
-                    if arg == 'd':
-                        expyriment.control.set_develop_mode(True)
-                    elif arg == 'i':
-                        print "* Intensive logging"
-                        expyriment.io.defaults.event_logging = 2
-                    elif arg == 'f':
-                        print "* Fast mode"
-                        expyriment.control.defaults.initialize_delay = 0
-                        expyriment.control.defaults.fast_quit = False
-                    elif arg == 'w':
-                        print "* Window mode (No OpenGL)"
-                        expyriment.control.defaults.open_gl = False
-                        expyriment.control.defaults.window_mode = True
-                    elif arg == 'g':
-                        print "* No OpenGL"
-                        expyriment.control.defaults.open_gl = False
-                    elif arg == 't':
-                        print "* No time stamps"
-                        expyriment.io.defaults.argvoutputfile_time_stamp = False
-                    elif arg == 'a':
-                        print "* Auto create subject id"
-                        expyriment.control.defaults.auto_create_subject_id = \
-                                True
-                    elif arg == "S":
-                        print "System Info"
-                        print expyriment.get_system_info(as_string=True)
+
+                # long options
+                if args.startswith("--"):
+                    if args == "--help":
+                        print info
                         sys.exit()
-                    elif arg == "T":
-                        print "Run Test Suite"
-                        expyriment.control.run_test_suite()
-                        sys.exit()
-                    elif arg == "A":
-                        print "Start API Reference Tool"
-                        expyriment.show_documentation(3)
-                        sys.exit()
-                    elif arg == "C":
-                        create_templet()
-                        sys.exit()
-                    elif arg == "J":
-                        join_data()
-                        sys.exit()
-                    elif arg == 'h':
+                    else:
+                        print unknown_option.format(args)
+
+                #short options
+                else:
+                    #sort args (capital letters last)
+                    arguments = list(args[1:])
+                    arguments.sort(reverse=True)
+
+                    for arg in arguments:
+                        if arg == 'd':
+                            expyriment.control.set_develop_mode(True)
+                        elif arg == 'i':
+                            print "* Intensive logging"
+                            expyriment.io.defaults.event_logging = 2
+                        elif arg == 'f':
+                            print "* Fast mode"
+                            expyriment.control.defaults.initialize_delay = 0
+                            expyriment.control.defaults.fast_quit = False
+                        elif arg == 'w':
+                            print "* Window mode (No OpenGL)"
+                            expyriment.control.defaults.open_gl = False
+                            expyriment.control.defaults.window_mode = True
+                        elif arg == 'g':
+                            print "* No OpenGL"
+                            expyriment.control.defaults.open_gl = False
+                        elif arg == 't':
+                            print "* No time stamps"
+                            expyriment.io.defaults.argvoutputfile_time_stamp =\
+                                    False
+                        elif arg == 'a':
+                            print "* Auto create subject id"
+                            expyriment.control.defaults.auto_create_subject_id =\
+                                    True
+                        elif arg == "S":
+                            print "System Info"
+                            print expyriment.get_system_info(as_string=True)
+                            sys.exit()
+                        elif arg == "T":
+                            print "Run Test Suite"
+                            expyriment.control.run_test_suite()
+                            sys.exit()
+                        elif arg == "A":
+                            print "Start API Reference Tool"
+                            expyriment.show_documentation(3)
+                            sys.exit()
+                        elif arg == "C":
+                            create_templet()
+                            sys.exit()
+                        elif arg == "J":
+                            join_data()
+                            sys.exit()
+                        elif arg == 'h':
                             print info
                             sys.exit()
+                        else:
+                            print unknown_option.format(arg)
+                            sys.exit()
 
+            #args starts not with "-"
             elif args.endswith(".py"):
                 script = args
 
