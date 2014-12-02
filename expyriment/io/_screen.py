36e7c29fd71e67d8fa59a43d18be1e025685005e
Oliver Lindemann <lindemann09@googlemail.com>
2014-12-02 11:49:52 +0100

    sync_mode

--- expyriment/io/_screen.py
+++ expyriment/io/_screen.py
@@ -34,7 +34,7 @@ class Screen(Output):
 
     """
 
-    def __init__(self, colour, open_gl, window_mode, window_size):
+    def __init__(self, colour, open_gl, window_mode, window_size, sync_mode=1):
         """Create and set up a screen output.
 
         Notes
@@ -53,6 +53,12 @@ class Screen(Output):
             size of the window in window_mode,
             full screen mode if size of window_mode[0]<=0
 
+        sync_mode : int
+            type of screen syncing
+            0: no screen syncing (always in non open_gl mode)
+            1: old sync xxxxx #TODO
+            2: new sync xxx #TODO
+
         """
 
         Output.__init__(self)
@@ -60,6 +66,11 @@ class Screen(Output):
         self._open_gl = open_gl
         self._fullscreen = not window_mode
         self._window_size = window_size
+        if open_gl: # TODO: Checking also Win & ATI here?
+            self._sync_mode = sync_mode
+        else:
+            self._sync_mode = 0
+
         if ogl is None:
             warn_message = "PyOpenGL is not installed. \
 OpenGL will be deactivated!"
@@ -129,6 +140,12 @@ machine!")
         return self._open_gl
 
     @property
+    def sync_mode(self):
+        """Getter for sync_mode."""
+
+        return self._sync_mode
+
+    @property
     def window_mode(self):
         """Getter for window_mode."""
 
@@ -155,16 +172,17 @@ machine!")
 
         pygame.event.pump()
         pygame.display.flip()
-        if self._open_gl:
-            ogl.glBegin(ogl.GL_POINTS)
-            ogl.glColor4f(0, 0, 0, 0)
-            vendor = ogl.glGetString(ogl.GL_VENDOR)
-            if sys.platform == 'win32' and vendor.lower().startswith('ati'):
-                pass
-            else:
-                # this corrupts text rendering on win with some ATI cards :-(
-                ogl.glVertex2i(0, 0)
-            ogl.glEnd()
+        if self._open_gl and self._sync_mode>0:
+            if self._sync_mode == 2:
+                ogl.glBegin(ogl.GL_POINTS)
+                ogl.glColor4f(0, 0, 0, 0)
+                vendor = ogl.glGetString(ogl.GL_VENDOR)
+                if sys.platform == 'win32' and vendor.lower().startswith('ati'): # TODO can't see be check when setting sync_mode in __init__
+                    pass
+                else:
+                    # this corrupts text rendering on win with some ATI cards :-(
+                    ogl.glVertex2i(0, 0)
+                ogl.glEnd()
             ogl.glFinish()
         if self._logging:
             expyriment._active_exp._event_file_log("Screen,updated", 2)
