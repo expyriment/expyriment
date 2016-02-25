fc4096f5ae3791ab4482fad0752d56289a167dfd
Florian Krause <florian.krause@fladd.de>
2016-02-25 13:15:45 +0100

    Docu: removed source links

--- documentation/sphinx/conf.py
+++ documentation/sphinx/conf.py
@@ -25,8 +25,10 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.viewcode',
-              'numpydoc']
+extensions = ['sphinx.ext.autodoc',
+             'sphinx.ext.doctest',
+             #'sphinx.ext.viewcode',
+             'numpydoc']
 
 # How to install numpydoc? --> sudo easy_install numpydoc
 
