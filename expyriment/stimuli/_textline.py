ef5187d021fa0f9ee6546d8dca0e258f186d6747
Florian Krause <fladd@web.de>
2016-08-01 17:37:27 +0200

    Update _textline.py

--- expyriment/stimuli/_textline.py
+++ expyriment/stimuli/_textline.py
@@ -276,7 +276,7 @@ class TextLine(Visual):
         _font.set_bold(self.text_bold)
         _font.set_italic(self.text_italic)
         _font.set_underline(self.text_underline)
-        if not isinstance(self.text, str):
+        if not _internals.is_unicode_string(self.text):
             # Pygame wants latin-1 encoding here for character strings
             _text = byte2unicode(self.text).encode('latin-1')
         else:
