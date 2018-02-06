1f3f97799e3c95878677328eda8352f2cac42177
Florian Krause <siebenhundertzehn@gmail.com>
2018-02-06 18:21:04 +0100

    Update _textline.py

--- expyriment/stimuli/_textline.py
+++ expyriment/stimuli/_textline.py
@@ -91,15 +91,16 @@ class TextLine(Visual):
             self._text_size = _internals.active_exp.text_size
         if text_font is None:
             text_font = defaults.textline_text_font
-        if text_font is not None:
-            self._text_font = find_font(text_font)
-        else:
-            self._text_font = find_font(_internals.active_exp.text_font)
+        if text_font is None:
+            text_font = _internals.active_exp.text_font
+        if text_font is None:
+            text_font = "FreeSans"
+        self._text_font = find_font(text_font)
         try:
             with open(self._text_font, 'rb') as f:
                 pygame.font.Font(f, 10)
         except:
-            raise IOError("Font '{0}' not found!".format(self._text_font))
+            raise IOError("Font '{0}' not found!".format(text_font))
         if text_bold is not None:
             self._text_bold = text_bold
         else:
