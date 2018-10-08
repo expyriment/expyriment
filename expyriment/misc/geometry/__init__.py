e1b074be4544fda39bc712a980ef59d190894fa2
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2018-10-08 12:28:20 +0200

    restructuring: geometry._basic_shapes.py

--- expyriment/misc/geometry/__init__.py
+++ expyriment/misc/geometry/__init__.py
@@ -16,5 +16,5 @@ from ._geometry import coordinates2position, position2coordinates, position2coor
 from ._geometry import position2visual_angle, visual_angle2position
 from ._geometry import cartesian2polar, polar2cartesian, tuples2points
 from ._geometry import points_to_vertices, points2vertices, lines_intersect, XYPoint
-from ._geometry import vertices_triangle, vertices_rectangle, vertices_regular_polygon, vertices_frame
-from ._geometry import vertices_trapezoid, vertices_parallelogram, vertices_cross
+from ._basic_shapes import vertices_triangle, vertices_rectangle, vertices_regular_polygon
+from ._basic_shapes import vertices_frame, vertices_trapezoid, vertices_parallelogram, vertices_cross
