1c8ac09e70c822e682fe10a46ca4bbe76515748e
fladd <florian.krause@fladd.de>
2023-11-07 00:46:16 +0100

    Update fullscreen_test.py

--- fullscreen_test.py
+++ fullscreen_test.py
@@ -4,9 +4,9 @@ import pygame
 
 xpy.control.defaults.display = 0
 xpy.control.defaults.open_gl = 3
-xpy.control.defaults.window_mode = True
-xpy.control.defaults.window_size = [800, 600] #[640, 480]
-xpy.control.defaults.window_scaling = 125
+#xpy.control.defaults.window_mode = True
+#xpy.control.defaults.window_size = [800, 600] #[640, 480]
+#xpy.control.defaults.window_scaling = 125
 #xpy.control.defaults.display_resolution = (1536, 864) #(1920, 1080)
 
 exp = xpy.control.initialize()
@@ -16,9 +16,16 @@ print("Window size", exp.screen.window_size)
 print("Display resolution", exp.screen.display_resolution)
 print("Num displays", pygame.display.get_num_displays())
 print("Desktop sizes", pygame.display.get_desktop_sizes())
+print("List modes", pygame.display.list_modes())
 size = exp.screen.window_size  #(1920, 1080) 
 #size = (1920, 1080) 
 rect = xpy.stimuli.Rectangle(size, line_width=1, colour=[255,0,0])
+rect.save("r.png")
 print(rect.present())
-exp.keyboard.wait()
+exp.screen.save("s.png")
+exp.keyboard.wait(xpy.misc.constants.K_SPACE)
+c = xpy.stimuli.Circle(100, line_width=2, colour=[255,0,0])
+c.present()
+c.save("c.png")
+exp.keyboard.wait(xpy.misc.constants.K_SPACE)
 #xpy.control.run_test_suite()
