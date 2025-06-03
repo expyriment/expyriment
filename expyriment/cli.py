38a79357aa41f1becbf56e958862a607625d9088
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-06-03 16:41:00 +0200

    Apply ruff/pycodestyle ruke E401
    E401 Multiple imports on one line

--- expyriment/cli.py
+++ expyriment/cli.py
@@ -11,7 +11,8 @@ __author__ = 'Florian Krause <florian@expyriment.org>, \
 Oliver Lindemann <oliver@expyriment.org>'
 
 
-import sys, os
+import os
+import sys
 from importlib import import_module
 from importlib.util import find_spec
 
